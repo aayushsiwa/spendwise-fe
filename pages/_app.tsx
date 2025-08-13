3bc6738df5450cb34ff9b8827ca2586a185e99cd
aayushsiwa <22052177@kiit.ac.in>
2025-08-13 08:36:09 +0530

    refactor: use Cat/Rec context provider in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
+import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { queryClient } from "./api";
+import { RecordsContextProvider } from "@/lib/context/Records/Records";
+import { CategoriesContextProvider } from "@/lib/context/Categories/Categories";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <CategoriesContextProvider>
+        <RecordsContextProvider>
+          <Component {...pageProps} />
+        </RecordsContextProvider>
+      </CategoriesContextProvider>
+    </QueryClientProvider>
+  );
 }
