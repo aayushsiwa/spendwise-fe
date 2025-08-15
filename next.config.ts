819ff25d6408f1d5539edce116faa384bbdae585
aayushsiwa <22052177@kiit.ac.in>
2025-08-15 14:22:13 +0530

    feat: hide actual api url behind proxy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async rewrites() {
+    // Rewrites only for local environment
+    if (process.env.ENVIRONMENT !== "local") {
+      return [];
+    }
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
