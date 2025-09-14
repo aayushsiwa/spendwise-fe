0474172fa512f05f800e30d0b037e6eb8d1732d5
aayushsiwa <22052177@kiit.ac.in>
2025-09-14 19:28:12 +0530

    feat: add toast

---
+++ lib/components/Toasts/Toast.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import Snackbar from "@mui/material/Snackbar";
+import type { SnackbarCloseReason } from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+
+const Toast: React.FC<{
+  variant: "success" | "error" | "info" | "warning";
+  message: string;
+  duration?: number;
+}> = ({ variant, message, duration = 6000 }) => {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: SnackbarCloseReason
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Snackbar open={open} autoHideDuration={duration} onClose={handleClose}>
+        <Alert
+          onClose={handleClose}
+          severity={variant}
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          {message}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+};
+
+export default Toast;
