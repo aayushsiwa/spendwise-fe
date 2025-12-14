8edb6b28746e1d148625ada207781cbe145d57c3
aayushsiwa <22052177@kiit.ac.in>
2025-12-14 15:01:49 +0530

    refactor: add query parameter types for pagination and time frame

---
+++ types/queryParams.ts
@@ -0,0 +1,35 @@
+export type PaginationQueryParams = {
+  page?: number;
+  limit?: number;
+};
+
+export enum TimeFrame {
+  Month = 'month',
+  Quarter = 'quarter',
+  Year = 'year',
+}
+
+export type TimeFrameQueryParams = {
+  timeframe?: TimeFrame;
+  year?: number;
+  quarter?: number;
+  month?: number;
+};
+
+export enum RecordType {
+  Income = 'income',
+  Expense = 'expense',
+  Transfer = 'transfer',
+}
+
+export type BaseQueryParams = TimeFrameQueryParams &
+  PaginationQueryParams & {
+    from?: Date;
+    to?: Date;
+    category?: string;
+    type?: RecordType;
+    minAmount?: number;
+    maxAmount?: number;
+    groupBy?: string;
+    search?: string;
+  };
