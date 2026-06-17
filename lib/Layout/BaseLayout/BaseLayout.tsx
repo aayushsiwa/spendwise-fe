433490a92dacb9cfaa3a9a14b91c719e71cc99f9
aayushsiwa <22052177@kiit.ac.in>
2026-06-17 08:54:54 +0530

    refactor: clean up BaseLayout - remove unused imports and PeriodProvider

--- lib/Layout/BaseLayout/BaseLayout.tsx
+++ lib/Layout/BaseLayout/BaseLayout.tsx
@@ -1,15 +1,7 @@
-import {
-  CloudUpload,
-  Dashboard as DashboardIcon,
-  Drafts as DraftsIcon,
-  Inbox as InboxIcon,
-  Send as SendIcon,
-  Settings as SettingsIcon,
-} from '@mui/icons-material';
+import { CloudUpload, Dashboard as DashboardIcon } from '@mui/icons-material';
 import { FC, PropsWithChildren } from 'react';
 
 import Sidebar, { SidebarMenuItem } from '@/lib/components/Sidebar/Sidebar';
-import { PeriodProvider } from '@/lib/context/Period/Period';
 import { SummaryProvider } from '@/lib/context/Summary/Summary';
 
 const BaseLayout: FC<PropsWithChildren> = ({ children }) => {
@@ -17,54 +9,26 @@ const BaseLayout: FC<PropsWithChildren> = ({ children }) => {
     {
       text: 'Dashboard',
       icon: <DashboardIcon />,
-      onClick: () => console.log('Dashboard clicked'),
       href: '/',
     },
     {
       text: 'Upload',
       icon: <CloudUpload />,
-      // onClick: () => console.log('Users clicked'),
       href: '/upload/csv',
-    },
-    {
-      text: 'Inbox',
-      icon: <InboxIcon />,
-      onClick: () => console.log('Inbox clicked'),
-      href: '/inbox',
-    },
-    {
-      text: 'Send Email',
-      icon: <SendIcon />,
-      onClick: () => console.log('Send Email clicked'),
-      href: '/send',
-    },
-    {
-      text: 'Drafts',
-      icon: <DraftsIcon />,
-      onClick: () => console.log('Drafts clicked'),
-      href: '/drafts',
-    },
-    {
-      text: 'Settings',
-      icon: <SettingsIcon />,
-      onClick: () => console.log('Settings clicked'),
-      href: '/settings',
     },
   ];
 
   return (
-    <PeriodProvider>
-      <SummaryProvider>
-        <Sidebar
-          title="Spend Wise"
-          menuItems={menuItems}
-          defaultOpen={false}
-          showAppBar={true}
-        >
-          {children}
-        </Sidebar>
-      </SummaryProvider>
-    </PeriodProvider>
+    <SummaryProvider>
+      <Sidebar
+        title="Spend Wise"
+        menuItems={menuItems}
+        defaultOpen={false}
+        showAppBar={true}
+      >
+        {children}
+      </Sidebar>
+    </SummaryProvider>
   );
 };
 
