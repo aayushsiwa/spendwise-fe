b4dfa64c2ba8768072cbdaf6b32c3ac601dcf995
aayushsiwa <22052177@kiit.ac.in>
2026-07-21 20:08:10 +0530

    feat: add MUI color scheme initialization to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,14 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" suppressHydrationWarning>
       <Head>
         <title>SpendWise</title>
       </Head>
       <body>
+        <InitColorSchemeScript attribute="data" />
         <Main />
         <NextScript />
       </body>
